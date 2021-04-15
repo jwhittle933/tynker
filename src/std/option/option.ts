970ef6c2c62b7dec2a1e244b70f4a477b588ca9b
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-04-14 21:31:53 -0400

    Result and Option work

--- src/std/option/option.ts
+++ src/std/option/option.ts
@@ -1,39 +1,75 @@
 import { cast } from '@/std/generics'
+import Results, { Result } from '@/std/result/result'
 
 // Module Interface
-export interface Options {
-    some: <T>(some: T) => Option<T>
-    none: () => Option<false>
+  export interface Options {
+  some: <T>(some: T) => Option<T>
+  none: () => Option
+}
+
+export interface OptionError {
+  message: string
+}
+
+export interface OptionMapper<T, M> {
+  (wrapped: T): M
 }
 
 interface OptionBase {
-    isSome: () => boolean
-    isNone: () => boolean
+  isSome: () => boolean
+  isNone: () => boolean
+  contains: <T>(val: T) => boolean
+  unwrap: <T>() => T | never
+  unwrapOr: <T>(def: T) => T
+  map: <T, M>(mapper: OptionMapper<T, M>) => Option<M> | Option
+  mapOr: <T, M>(def: M, mapper: OptionMapper<T, M>) => M
 }
 
 export interface Option<T = false> extends OptionBase {
-    Some?: T
-    None?: boolean
+  Some?: T
+  None?: boolean
 }
-
-const some = <T>(some: T): Option<T> => {
-    return { Some: some, None: false }
-}
-
-const none = (): Option => {
-    return { None: true }
-}
-
 function isSome<T>(this: Option<T>): boolean {
-    return !this.None!
+  return !this.None!
 }
 
 function isNone<T>(this: Option<T>): boolean {
-    return this.None!
+  return this.None!
+}
+
+function unwrap<T>(this: Option<T>): T | never {
+  if (this.isSome()) return this.Some!
+
+  throw cast<OptionError>({ message: 'Option is None' })
+}
+
+function unwrapOr<T>(this: Option<T>, def: T): T {
+  if (this.isSome()) return this.Some!
+  return def
+}
+
+function map<T, M>(this: Option<T>, mapper: OptionMapper<T, M>): Option<M> | Option {
+  if (this.isSome()) return some<M>(mapper(this.Some!))
+  return none()
+}
+
+function mapOr<T, M>(this: Option<T>, def: M, mapper: OptionMapper<T, M>): M {
+  if(this.isSome()) return mapper(this.Some!)
+  return def
 }
 
 function contains<T>(this: Option<T>, val: T): boolean {
-    return this.isSome() && this.Some! === val
+  return this.isSome() && this.Some! === val
+}
+
+const optionBase = { isSome, isNone, unwrap, unwrapOr, map, mapOr, contains }
+
+const some = <T>(some: T): Option<T> => {
+  return { Some: some, None: false, ...optionBase }
+}
+
+const none = (): Option => {
+  return { None: true, ...optionBase }
 }
 
 export default cast<Options>({ some, none })
--- src/std/result/result.ts
+++ src/std/result/result.ts
@@ -3,89 +3,88 @@ import { cast } from '@/std/generics'
 
 // Module Interface
 export interface Results {
-    newOk: <T>(data: T) => Result<T, unknown>
-    newError: <E>(error: E) => Result<unknown, E>
+  newOk: <T>(data: T) => Result<T, unknown>
+  newError: <E>(error: E) => Result<unknown, E>
 }
 
 // `Result` wraps two potential values, Ok, and Err
 export interface Result<T, E> {
-    ok: () => Option<T>
-    isOk: () => boolean
-    asOk: () => Ok<T>
-    err: () => Option<E>
-    isErr: () => boolean
-    asErr: () => Err<E>
-    contains: (val: T) => boolean
-    containsErr: (err: E) => boolean
-    unwrap: () => T | undefined
-    unwrapOr: (def: T) => T
-    unwrapErr: () => E | undefined
-    expect: (msg: string) => T | never
-    next: (nextFunc: { (data: T): Result<T, E> }) => Result<T, E>
+  ok: () => Option<T>
+  isOk: () => boolean
+  asOk: () => Ok<T>
+  err: () => Option<E>
+  isErr: () => boolean
+  asErr: () => Err<E>
+  contains: (val: T) => boolean
+  containsErr: (err: E) => boolean
+  unwrap: () => T | undefined
+  unwrapOr: (def: T) => T
+  unwrapErr: () => E | undefined
+  expect: (msg: string) => T | never
+  map: (mapper: { (data: T): Result<T, E> }) => Result<T, E>
 }
 
 // Ok extends Result (and can be used in its place)
 // The Ok interface wraps the result of a successful
 // operation
 interface Ok<T> extends Result<T, unknown> {
-    data: T
+  data: T
 }
 
 // Err extends Result (and can be used in its place)
 // The Err interface wraps the result of a failed
 // operation
 interface Err<E> extends Result<unknown, E> {
-    error: E
+  error: E
 }
 
 function ok<T, E>(this: Result<T, E>): Option<T | false> {
-    if (this.isOk()) return Options.some<T>(this.unwrap()!)
-    return Options.none()
+  if (this.isOk()) return Options.some<T>(this.unwrap()!)
+  return Options.none()
 }
 
 function isOk<T, E>(this: Result<T, E>): boolean {
-    return !!(<Ok<T>>this)
+  return !!(<Ok<T>>this)
 }
 
 function asOk<T>(this: Result<T, unknown>): Ok<T> {
-    return <Ok<T>>this
+  return <Ok<T>>this
 }
 
 function err<T, E>(this: Result<T, E>): Option<E | false> {
-    if (this.isErr()) return Options.some<E>(this.unwrapErr()!)
-    return Options.none()
+  if (this.isErr()) return Options.some<E>(this.unwrapErr()!)
+  return Options.none()
 }
 
 function isErr<T, E>(this: Result<T, E>): boolean {
-    return !!(<Err<E>>this)
+  return !!(<Err<E>>this)
 }
 
 function asErr<E>(this: Result<unknown, E>): Err<E> {
-    return <Err<E>>this
+  return <Err<E>>this
 }
 
 function contains<T, E>(this: Result<T, E>, val: T): boolean {
-    return this.isOk() && this.asOk().data === val
+  return this.isOk() && this.asOk().data === val
 }
 
 function containsErr<T, E>(this: Result<T, E>, err: E): boolean {
-    return this.isErr() && this.asErr().error === err
+  return this.isErr() && this.asErr().error === err
 }
 
-
 function unwrap<T, E>(this: Result<T, E>): T | undefined {
-    if (this.isOk()) return this.asOk().data
-    return undefined
+  if (this.isOk()) return this.asOk().data
+  return undefined
 }
 
 function unwrapOr<T, E>(this: Result<T, E>, def: T): T {
-    if (this.isOk()) return this.asOk().data
-    return def
+  if (this.isOk()) return this.asOk().data
+  return def
 }
 
 function unwrapErr<T, E>(this: Result<T, E>): E | undefined {
-    if (this.isErr()) return this.asErr().error
-    return undefined
+  if (this.isErr()) return this.asErr().error
+  return undefined
 }
 
 // expect asserts that the data exists and returns that data,
@@ -93,57 +92,43 @@ function unwrapErr<T, E>(this: Result<T, E>): E | undefined {
 // Should be used only when you know for certain that the operation
 // succeeded, or within a try/catch
 function expect<T>(this: Result<T, unknown>, msg: string): T | never {
-    if (this.isOk()) return this.unwrap()!
+  if (this.isOk()) return this.unwrap()!
 
-    throw new Error(msg)
+  throw new Error(msg)
 }
 
 // next in functional programming is often called `bind`
 // The wrapped data will be unwrapped and passed to the `nextFunc`,
 // unless the Result is an error and the Result will be returned
 // The `nextFunc` must return a new Result, either newOk or newError
-function next<T, E>(this: Result<T, E>, nextFunc: { (data: T): Result<T, E> }): Result<T, E> {
-    if(this.isErr()) return this
+function map<T, E>(this: Result<T, E>, mapper: { (data: T): Result<T, E> }): Result<T, E> {
+  if (this.isErr()) return this
 
-    return nextFunc(this.unwrap()!)
+  return mapper(this.unwrap()!)
+}
+
+const resultBase = {
+  ok,
+  isOk,
+  asOk,
+  err,
+  isErr,
+  asErr,
+  contains,
+  containsErr,
+  unwrap,
+  unwrapOr,
+  unwrapErr,
+  expect,
+  map,
 }
 
 const newError = <E>(error: E): Result<unknown, E> => {
-    return <Err<E>>{
-        error,
-        ok,
-        isOk,
-        asOk,
-        err,
-        isErr,
-        asErr,
-        contains,
-        containsErr,
-        unwrap,
-        unwrapOr,
-        unwrapErr,
-        expect,
-        next
-    }
+  return <Err<E>>{ error, ...resultBase }
 }
 
 const newOk = <T>(data: T): Result<T, unknown> => {
-    return <Ok<T>>{
-        data,
-        ok,
-        isOk,
-        asOk,
-        err,
-        isErr,
-        asErr,
-        contains,
-        containsErr,
-        unwrap,
-        unwrapOr,
-        unwrapErr,
-        expect,
-        next
-    }
+  return <Ok<T>>{ data, ...resultBase }
 }
 
 export default cast<Results>({ newOk, newError })
