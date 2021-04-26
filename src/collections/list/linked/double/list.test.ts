321e2b679d122111d4da84b68ae0392ca8b09e54
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-04-26 07:58:14 -0400

    start doubly linked tests

--- src/collections/list/linked/double/list.test.ts
+++ src/collections/list/linked/double/list.test.ts
@@ -4,7 +4,7 @@ import DLL from './list'
 import Nodes from '@/primitive/node'
 
 describe.only('DoublyLinkedList', () => {
-  describe('insertHead', () => {
+  describe('create', () => {
     describe('When list is created with 1 value', () => {
       const actual = DLL.create(10)
 
@@ -18,6 +18,24 @@ describe.only('DoublyLinkedList', () => {
 
       it('should have a null right node', () => {
         expect(actual.right).to.be.null
+      })
+
+      it('should have a null left node', () => {
+        expect(actual.left).to.be.null
+      })
+    })
+  })
+
+  describe('insertHead', () => {
+    describe('When a new value is inserted at the head', () => {
+      const actual = DLL.create(10).insertHead(20)
+
+      it('should have that value at the head', () => {
+        expect(actual.value).to.equal(20)
+      })
+
+      it('should have the previous head in the second position', () => {
+        expect(actual.right!.value).to.equal(10)
       })
 
       it('should have a null left node', () => {
